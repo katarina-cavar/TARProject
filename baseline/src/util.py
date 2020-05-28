05c56334b0c7ea20dd41b6b367b41da83f399ca3
Sanja Deur <sanja.deur@fer.hr>
2020-05-28 22:21:06 +0200

    MTSA dataset divided into train and test set

--- baseline/src/util.py
+++ baseline/src/util.py
@@ -3,8 +3,27 @@ import random
 
 # e.g. fraction = 0.8 -> train set: 80%
 #                     -> validation set: 20%
-def partition(data, fraction):
-    ldata = list(data)
+def partition(ldata, fraction):
+    # ldata = list(data)
     random.shuffle(ldata)
     breakPoint = int(len(ldata) * fraction)
     return ldata[:breakPoint], ldata[breakPoint:]
+
+
+data = []
+with open('../../Dataset-MTSA/extracted_tweets.tsv', encoding='utf-8') as fin:
+    for cnt, line in enumerate(fin):
+        if cnt == 0:
+            header = line
+            continue
+        data.append(line)
+
+train, test = partition(data, 0.935)
+print(len(train), len(test))
+with open('../../Dataset-MTSA/tweets_train.tsv', 'w', encoding='utf-8') as fout:
+    fout.write(header)
+    [fout.write(t) for t in train]
+
+with open('../../Dataset-MTSA/tweets_test.tsv', 'w', encoding='utf-8') as fout:
+    fout.write(header)
+    [fout.write(t) for t in test]
